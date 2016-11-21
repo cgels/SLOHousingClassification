adda9871a77d2bdc9eb066a4628cf1e90a327ef7
cguls <cgels@calpoly.edu>
2016-11-21 12:42:43 -0800

    added catch for missing SqFootage cell in scraper

--- house_scaper.py
+++ house_scaper.py
@@ -5,7 +5,7 @@ import requests
 import sqlite3
 import datetime
 
-df = pd.DataFrame({"MLS": [],"Street": [], "City":[],"List Price":[],"Bedrooms":[],"Bathrooms":[],"Square Footage":[],"Date":[],  "Price/SqFt":[]})
+df = pd.DataFrame({"MLS": [],"Street": [], "City":[],"ListPrice":[],"Bedrooms":[],"Bathrooms":[],"SqFt":[],"Date":[],  "Price/SqFt":[]})
 months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
               "November", "December"]
 month_map = {key: int(val) for key, val in zip(months, range(1, 13))}
@@ -21,22 +21,21 @@ def get_date(string):
 def scraping(dataframe):
     page = requests.get("http://www.slocountyhomes.com/newlistex.php")
     data = BeautifulSoup(page.text, "html.parser")
-    hdrs = ["Bathrooms", "Bedrooms", "City", "Date", "List Price", "MLS", "Price/SqFt", "Square Footage", "Street"]
+    hdrs = ["Bathrooms", "Bedrooms", "City", "Date", "List Price", "MLS", "Price/SqFt", "SqFt", "Street"]
     idx_map = { hdr:idx for hdr, idx in zip(hdrs, range(len(hdrs))) }
-    row_entry = [0] * len(hdrs)
 
 
-    table_rows = data.find("tbody").find_all("tr")
-
+    table_rows = data.find_all('tr')
+    # print(table_rows)
+    listing_date = ""
     for row in table_rows:
-        listing_date = ""
-        cells = row.find_all("td")
+        row_entry = [0] * len(hdrs)
+        cells = row.find_all("td", recursive=True)
         if len(cells) == 1:
-            listing_date = cells[0].text
-
-        else:
+            listing_date = cells[0].text.strip()
             assert listing_date != ""
-            assert len(cells) == 8
+
+        elif 0 < len(cells) <= 8 and len(cells) != 3:
             ## CELL ORDER -->   MLS #	Street	City	List Price	Beds	Baths	Sq Footage
             row_entry[idx_map["MLS"]] = int(cells[0].text.strip())
             row_entry[idx_map["Street"]] = sqlize_string(cells[1].text.strip())
@@ -44,11 +43,19 @@ def scraping(dataframe):
             row_entry[idx_map["List Price"]] = int(cells[3].text.strip()[1:].replace(",", ""))
             row_entry[idx_map["Bedrooms"]] = int(cells[4].text.strip())
             row_entry[idx_map["Bathrooms"]] = int(cells[5].text.strip())
-            row_entry[idx_map["Square Footage"]] = int(cells[6].text.strip())
+            try: # handle missing Sq footage
+                row_entry[idx_map["SqFt"]] = int(cells[6].text.strip())
+                row_entry[idx_map["Price/SqFt"]] = row_entry[idx_map["List Price"]] / row_entry[
+                    idx_map["SqFt"]]
+            except ValueError:
+                row_entry[idx_map["SqFt"]] = -1
             row_entry[idx_map["Date"]] = get_date(listing_date)
-            row_entry[idx_map["Price/SqFt"]] = row_entry[idx_map["List Price"]] / row_entry[idx_map["Square Footage"]]
             ## append this row to dataframe
+            # print(row_entry)
             dataframe.loc[len(dataframe)] = row_entry
-
-    dataframe = dataframe.drop_duplicates()
+    ## data integrity
+    dataframe.drop(dataframe[dataframe.SqFt == -1].index, inplace=True)
+    dataframe.drop_duplicates(inplace=True)
     return dataframe
+
+df = scraping(df)
