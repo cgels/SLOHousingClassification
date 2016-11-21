08d785cb8eac920746ef97caaff363e91a51ea4a
cguls <cgels@calpoly.edu>
2016-11-21 13:12:12 -0800

    slo_housing database work

--- sqlite_api.py
+++ sqlite_api.py
@@ -1,25 +1,26 @@
 import sqlite3
 import datetime
 
-class Database:
+class SLOHouseDatabase:
 
     def __init__(self):
         self.database = 'slo_housing.db'
-        self.insertStmt = "INSERT INTO HOUSES (MLS_ID,BED,BATH,CITY, ADDRESS, LIST_PRICE, SQ_FOOTAGE, PRICE_PER_SQFT, LIST_DATE) \
+        self.insertStmt = "INSERT OR REPLACE INTO HOUSES (MLS_ID,BED,BATH,CITY, ADDRESS, LIST_PRICE, SQ_FOOTAGE, PRICE_PER_SQFT, LIST_DATE) \
           VALUES ({}, {}, {}, {}, {}, {}, {}, {}, {})"
-        self.deleteStmt = "DELETE FROM HOUSES WHERE "
-        self.month_map = self._get_month_map()
 
-
-    def _get_month_map(self):
-        months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October",
-                  "November", "December"]
-        return {key: int(val) for key, val in zip(months, range(1, 13))}
-
-    def insert_row(self, row):
+    def _insert_row(self, conn, row):
         conn = sqlite3.connect(self.database)
         conn.execute(self.insertStmt.format(row['MLS'], row['Bedrooms'], row['Bathrooms'], row['City'],
                                  row['Street'], row['List Price'], row['Square Footage'],
                                  int(row['List Price']) / int(row['Square Footage']),
                                  datetime.date(2016, self.month_map[row['Date'].split()[0]],
                                  int(row['Date'].split()[1]))))
+
+    def insert_dataframe(self, df):
+        connect = sqlite3.connect(self.database)
+        print("Connected to " + self.database)
+        for idx, row in df.iterrows():
+            self._insert_row(connect, row)
+        connect.commit()
+        print("Records created successfully")
+        connect.close()
